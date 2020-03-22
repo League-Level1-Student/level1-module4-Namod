d3dbd1d37705a59049a3995427463b482b0c85c6
Namod <46327298+Namod@users.noreply.github.com>
2020-03-22 10:15:47 -0700

    Update Chuckle.java

--- src/_04_chuckle_clicker/Chuckle.java
+++ src/_04_chuckle_clicker/Chuckle.java
@@ -1,5 +1,36 @@
 package _04_chuckle_clicker;
 
-public class Chuckle {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
+public class Chuckle {
+public static void main(String[] args) {
+	
+	makeButtons();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	frame.add(panel);
+	frame.add(button1);
+	frame.add(button2);
+	frame.setTitle("Chuckle Clicker");
+	button1.setText("Joke");
+	button2.setText("Punchline");
+	button1.addActionListener(this);
+	button2.addActionListener(this);
+	if(actionListener) == button1 && actionListener == button2){
+		JOptionPane.showMessageDialog(null, "Hi");
+		
+	}
+		JOptionPane.showMessageDialog(null,"Make Buttons");
+	 Chuckle chuck = new Chuckle();
+}
+
+private static void makeButtons() {
+	// TODO Auto-generated method stub
+	
+}
 }
