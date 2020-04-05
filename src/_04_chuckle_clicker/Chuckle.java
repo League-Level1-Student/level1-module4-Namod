4c298abd98662061a477d5edad77c264301ba0b4
Namod <46327298+Namod@users.noreply.github.com>
2020-04-05 11:28:01 -0700

    a

--- src/_04_chuckle_clicker/Chuckle.java
+++ src/_04_chuckle_clicker/Chuckle.java
@@ -1,23 +1,19 @@
 package _04_chuckle_clicker;
-
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Chuckle implements ActionListener {
-
-void makeButtons() {
-	// TODO Auto-generated method stub
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
-	
+void makeButtons() {
+	// TODO Auto-generated method stu
+
 	frame.add(panel);
 	frame.add(button1);
 	frame.add(button2);
@@ -26,19 +22,24 @@ void makeButtons() {
 	button2.setText("Punchline");
 	button1.addActionListener(this);
 	button2.addActionListener(this);
+	frame.pack();
+	frame.setVisible(true);
+	
 }
 
+@SuppressWarnings("unlikely-arg-type")
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	
+e.getSource();
+	if(e.equals(button1)) {
+		JOptionPane.showMessageDialog(null, "When does a joke become a 'Dad Joke'?");
+		{
+	if(e.equals(button2)) {	
+			JOptionPane.showMessageDialog(null, "When the punchline becomes A-Parent");
+		}
+		}
+	}
+		
 }
-if(ActionListener == button1) {
-	System.out.println("Joke");
 }
-else if(ActionListener == button2) {
-	System.out.println("Puncnline");
-}
-
-}
-//}
--- src/_04_chuckle_clicker/ChuckleRunner.java
+++ src/_04_chuckle_clicker/ChuckleRunner.java
@@ -1,11 +1,8 @@
 package _04_chuckle_clicker;
 
-import java.awt.event.ActionListener;
-
 public class ChuckleRunner {
 public static void main(String[] args) {
-	new Chuckle().makeButtons();
-	if(event.getSource() == button1) {
-}
+	Chuckle x = new Chuckle();
+	x.makeButtons();
 }
 }
